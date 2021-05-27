84ceeb4e02db399729e113f2beebe56e2e7e99eb
Azam2012 <ghasemighasemi195@gmail.com>
2021-05-27 15:53:00 +0200

    Added js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
 
+let apiKey = "23299a9890a18af9b64db3fe1ab69a3a";
+let apiUrl = `api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
