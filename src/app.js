459b9411933ce6c3e937fb71d8cf621058ea85fe
Azam2012 <ghasemighasemi195@gmail.com>
2021-06-25 16:45:48 +0200

    added Html to JS

--- src/app.js
+++ src/app.js
@@ -15,10 +15,49 @@ function formatDate(timestamp) {
     "Wednsday",
     "Thursday",
     "Friday",
-    "Saturday",
+    "Saturday"
   ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
+}
+function displayForecast( ) {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = [   "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri"
+]
+let forecastHTML = `<div class="row">`;
+    days.forEach(function(day){
+     
+  forecastHTML =   forecastHTML  + `
+   
+              <div class="col-2">
+                <div class="weahter-forecast-date">
+                
+              ${day}
+                </div>
+<img 
+src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png" alt="" width="40">
+<div class="weather-forecast-temperature">
+  <span class="weather-forecast-temperature-max"> <strong>18°</strong>
+    </span>
+<span class="weather-forecast-temperature-min"> 12°</span>
+  
+</div>
+                 
+              </div>
+            
+            `;
+
+    })
+  
+            forecastHTML  = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+
 }
 
 function displayTemperature(response) {
@@ -86,3 +125,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Berlin");
+displayForecast();
