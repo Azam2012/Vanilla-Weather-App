5c5c9a1a903f21b1a34f767e03e612d86c177217
Azam2012 <ghasemighasemi195@gmail.com>
2021-06-26 22:26:06 +0200

    Display forecast data using API

--- src/app.js
+++ src/app.js
@@ -20,44 +20,57 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-function displayForecast( ) {
-  let forecastElement = document.querySelector("#forecast");
 
-  let days = [   "Sun",
-  "Mon",
-  "Tue",
-  "Wed",
-  "Thu",
-  "Fri"
-]
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu","Fri", "Sat"]
+
+return days[day] ;
+
+
+}
+
+function displayForecast(response ) {
+let forecast =  response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+ 
+ 
 let forecastHTML = `<div class="row">`;
-    days.forEach(function(day){
+    forecast.forEach(function(forecastDay,  index){
+      if  (index <6){  
      
   forecastHTML =   forecastHTML  + `
    
               <div class="col-2">
                 <div class="weahter-forecast-date">
                 
-              ${day}
+              ${formatDay(forecastDay.dt)}
                 </div>
 <img 
-src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png" alt="" width="40">
+src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="40">
 <div class="weather-forecast-temperature">
-  <span class="weather-forecast-temperature-max"> <strong>18°</strong>
+  <span class="weather-forecast-temperature-max"> <strong>${Math.round(forecastDay.temp.max)}°</strong>
     </span>
-<span class="weather-forecast-temperature-min"> 12°</span>
+<span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}°</span>
   
 </div>
                  
               </div>
             
-            `;
+            `;}
 
     })
   
             forecastHTML  = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 
+}
+function getForecast(coordinates) {
+   let apiKey = "23299a9890a18af9b64db3fe1ab69a3a";
+  let apiUrl =` https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast)
+  
 }
 
 function displayTemperature(response) {
@@ -82,7 +95,9 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+getForecast(response.data.coord); 
 }
+
 function search(city) {
   let apiKey = "23299a9890a18af9b64db3fe1ab69a3a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -125,4 +140,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Berlin");
-displayForecast();
+
